0659ba7c805de1efbe9ecd1d9f07a0617ca8baa8
kdhameliya74 <kdhameliya94@gmail.com>
2025-11-27 14:11:19 +0530

    feat: production build

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 // Entry Point
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import {
+  ApolloServerPluginLandingPageLocalDefault,
+  ApolloServerPluginLandingPageProductionDefault
+} from '@apollo/server/plugin/landingPage/default';
 
 // Type Definitions
 import typeDefs from '@/schema/typeDefs';
 // Resolvers
 import resolvers from '@/schema/resolvers';
 
+const dev = process.env.NODE_ENV !== 'production';
 const PORT = 8080;
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  introspection: true, // enable introspection in all environments for learning/demo purposes
+  plugins: [
+    dev
+      ? ApolloServerPluginLandingPageLocalDefault()
+      : ApolloServerPluginLandingPageProductionDefault({
+          embed: true, // shows embedded Apollo Sandbox UI
+        }),
+  ],
 });
 
 const { url } = await startStandaloneServer(server, {
